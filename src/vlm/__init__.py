f80fcb2d198ee9ed5cd19634c7c88f154d56d3d3
Nguyen Trong Phuc <trongphuc.nguyen.cs@gmail.com>
2026-09-04 19:21:06 +0700

    feat: export VLM V2 prompt and image binding API

--- src/vlm/__init__.py
+++ src/vlm/__init__.py
@@ -6,6 +6,13 @@ from .pipeline import (
     render_explanation_vi,
     validate_visual_analysis,
 )
+from .prompt_v2 import (
+    VISUAL_ANALYSIS_SCHEMA_VERSION_V2,
+    append_repair_request_v2,
+    build_qwen_messages_v2,
+    expected_output_shape_v2,
+    required_limitations_v2,
+)
 from .schema import (
     EVIDENCE_SCHEMA_VERSION,
     build_vlm_evidence,
@@ -28,14 +35,19 @@ __all__ = [
     "EVIDENCE_SCHEMA_VERSION",
     "EVIDENCE_SCHEMA_VERSION_V2",
     "GROUNDING_RULES_V2",
+    "VISUAL_ANALYSIS_SCHEMA_VERSION_V2",
     "VLMExplanationPipeline",
     "VLM_PROTOCOL_VERSION",
+    "append_repair_request_v2",
+    "build_qwen_messages_v2",
     "build_recommendation_evidence",
     "build_vlm_evidence",
     "build_vlm_evidence_v2",
     "canonical_evidence_json_v2",
+    "expected_output_shape_v2",
     "load_vlm_config",
     "render_explanation_vi",
+    "required_limitations_v2",
     "validate_recommendation_evidence",
     "validate_vlm_evidence",
     "validate_vlm_evidence_v2",
